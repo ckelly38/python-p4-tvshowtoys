bf3cd9d74fc0f8daf0b38699067a3262c21d24f9
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2024-02-09 18:11:20 -0700

    this adds some basic general code needed for the server

--- server/app.py
+++ server/app.py
@@ -27,6 +27,65 @@ from models import User, Show, Episode, Toy, UserToy, UserEpisodes
 #a user should be able to change their watch history
 #a user should be able to get rid of purchased toys (sell, donate, or throw them out)
 
+class Commonalities:
+    def getValidClassList(self):
+        return [User, Show, Episode, Toy, UserToy, UserEpisodes];
+
+    def isClsValid(self, cls):
+        if (cls == None): return False;
+        else: return (cls in self.getValidClassList());
+
+    def getTypeStringForClass(self, cls):
+        if (cls == None): raise ValueError("the class must not be null and None!");
+        elif (cls == User): return "User";
+        elif (cls == Show): return "Show";
+        elif (cls == Episode): return "Episode";
+        elif (cls == Toy): return "Toy";
+        elif (cls == UserToy): return "UserToy";
+        elif (cls == UserEpisodes): return "UserEpisodes";
+        else:
+            raise ValueError("the class must be one of the following: " +
+                             f"{self.getValidClassList()}!");
+
+    def getAllOfTypeFromDB(self, cls):
+        if (self.isClsValid(cls)): return cls.query.all();
+        else:
+            raise ValueError("the class must be one of the following: " +
+                             f"{self.getValidClassList()}!");
+
+    def getSerializedItem(self, cls, item):
+        if (item == None): raise ValueError("the item object must not be null or None!");
+        if (self.isClsValid(cls)): return item.to_dict(only=cls.full_list);
+        else:
+            raise ValueError("the class must be one of the following: " +
+                             f"{self.getValidClassList()}!");
+
+    def getSerializedItemOnly(self, item):
+        if (item == None): raise ValueError("the item object must not be null or None!");
+        else: return self.getSerializedItem(type(item), item);
+
+    def getAllOfTypeAndSerializeThem(self, cls):
+        return [self.getSerializedItem(cls, item) for item in self.getAllOfTypeFromDB(cls)];
+
+    def getItemByID(self, id, cls):
+        if (id == None or cls == None):
+            raise ValueError("id and cls must not be null or None!");
+        elif (type(id) != int): raise ValueError("id must be an integer!");
+        elif (self.isClsValid(cls)): return cls.query.filter_by(id=id).first();
+        else:
+            raise ValueError("the class must be one of the following: " +
+                             f"{self.getValidClassList()}!");
+
+    def getItemByIDAndReturnResponse(self, id, cls):
+        item = self.getItemByID(id, cls);
+        if (item == None):
+            errmsg = f"404 error item of type {self.getTypeStringForClass(cls)}";
+            errmsg += f", with id {id} not found!";
+            return {"error": errmsg}, 404;
+        else: return self.getSerializedItem(cls, item), 200;
+
+cm = Commonalities();
+
 class Users(Resource):
     def get(self):
         pass;
@@ -48,39 +107,18 @@ class UsersByID(Resource):
 
 #api.add_resource(Class, "url");
 
-class Shows(Resource):
-    def get(self):
-        pass;
-
-    def post(self):
-        pass;
-
-#api.add_resource(Class, "url");
-
-class ShowsById(Resource):
-    def get(self, id):
-        pass;
-
-    def patch(self, id):
-        pass;
-
-    def delete(self, id):
-        pass;
-
-#api.add_resource(Class, "url");
-
 class Episodes(Resource):
     def get(self):
-        pass;
+        return cm.getAllOfTypeAndSerializeThem(Episode), 200;
 
     def post(self):
         pass;
 
-#api.add_resource(Class, "url");
+api.add_resource(Episodes, "/shows/<int:showid>/episodes");
 
 class EpisodesByID(Resource):
     def get(self, id):
-        pass;
+        return cm.getItemByIDAndReturnResponse(id, Episode);
 
     def patch(self, id):
         pass;
@@ -88,20 +126,20 @@ class EpisodesByID(Resource):
     def delete(self, id):
         pass;
 
-#api.add_resource(Class, "url");
+api.add_resource(EpisodesByID, "/shows/<int:showid>/episodes/<int:id>");
 
-class Toys(Resource):
+class Shows(Resource):
     def get(self):
-        pass;
+        return cm.getAllOfTypeAndSerializeThem(Show), 200;
 
     def post(self):
         pass;
 
-#api.add_resource(Class, "url");
+api.add_resource(Shows, "/shows");
 
-class ToysByID(Resource):
+class ShowsById(Resource):
     def get(self, id):
-        pass;
+        return cm.getItemByIDAndReturnResponse(id, Show);
 
     def patch(self, id):
         pass;
@@ -109,7 +147,28 @@ class ToysByID(Resource):
     def delete(self, id):
         pass;
 
-#api.add_resource(Class, "url");
+api.add_resource(ShowsById, "/shows/<int:id>");
+
+class Toys(Resource):
+    def get(self):
+        return cm.getAllOfTypeAndSerializeThem(Toy), 200;
+
+    def post(self):
+        pass;
+
+api.add_resource(Toys, "/toys");
+
+class ToysByID(Resource):
+    def get(self, id):
+        return cm.getItemByIDAndReturnResponse(id, Toy);
+
+    def patch(self, id):
+        pass;
+
+    def delete(self, id):
+        pass;
+
+api.add_resource(ToysByID, "/toys/<int:id>");
 
 #not sure if these are needed or not...
 
