2d918311654d9959b84448e5c5b8e2013717d383
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2024-02-21 17:05:47 -0700

    this hides buttons from the user that do not function if the user is not logged in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, useParams } from "react-router-dom";
 
-function Navbar({username="not logged in"}) {
-  return (<div className="navbarcls">
-    <div className="homelinkcls"><Link to="/">Home</Link></div>
-    <div className="showslinkcls"><Link to="/shows">Shows</Link></div>
-    <button onClick={null}>New Show</button>
-    <div className="epslinkcls"><Link to="/shows/:showid/episodes">Episodes</Link></div>
-    <button onClick={null}>New Episode</button>
-    <div className="toyslinkcls"><Link to="/toys">Toys</Link></div>
-    <button onClick={null}>New Toy</button>
-    <div className="toysforshowlinkcls"><Link to="/shows/:showid/toys">Toys For Show</Link></div>
-    <div className="mytoyslinkcls"><Link to="/my-toys">My Toys</Link></div>
-    <div className="myepslinkcls"><Link to="/my-episodes">My Episodes</Link></div>
-    <div className="loginlinkcls"><Link to="/login">Login</Link></div>
-    <div className="logoutlinkcls"><Link to="/logout">Logout</Link></div>
-    <div className="signuplinkcls"><Link to="/signup">Signup</Link></div>
-    <div className="prefslinkcls"><Link to="/preferences">
-        <div id="username">Preferences: {username}</div>
-    </Link></div>
-  </div>);
+function Navbar({username="not logged in", notloggedin=true}) {
+    return (<div className="navbarcls">
+        <div className="homelinkcls"><Link to="/">Home</Link></div>
+        <div className="showslinkcls"><Link to="/shows">Shows</Link></div>
+        {(notloggedin) ? null : <button onClick={null}>New Show</button>}
+        <div className="epslinkcls"><Link to="/shows/:showid/episodes">Episodes</Link></div>
+        {(notloggedin) ? null : <button onClick={null}>New Episode</button>}
+        <div className="toyslinkcls"><Link to="/toys">Toys</Link></div>
+        {(notloggedin) ? null : <button onClick={null}>New Toy</button>}
+        <div className="toysforshowlinkcls"><Link to="/shows/:showid/toys">Toys For Show</Link>
+        </div>
+        {(notloggedin) ? null : <><div className="mytoyslinkcls">
+          <Link to="/my-toys">My Toys</Link></div>
+          <div className="myepslinkcls"><Link to="/my-episodes">My Episodes</Link></div></>}
+        {(notloggedin) ? <div className="loginlinkcls"><Link to="/login">Login</Link></div> :
+        <div className="logoutlinkcls"><Link to="/logout">Logout</Link></div>}
+        <div className="signuplinkcls"><Link to="/signup">Signup</Link></div>
+        <div className="prefslinkcls"><Link to="/preferences">
+            <div id="username">Preferences: {username}</div>
+        </Link></div>
+    </div>);
 }
 
 export default Navbar;
